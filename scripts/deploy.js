f0c46018b7c69583997c578839a6836fb5954864
Kelvin <quocvm1@gmail.com>
2022-04-08 22:33:18 +0800

    init repo minting nft

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat"); 
+
+async function main() {  
+
+    const horoscopeNFT = await hre.ethers.getContractFactory("horoscopeNFT");
+    const hscp = await horoscopeNFT.deploy();   
+    await hscp.deployed();
+	
+	//since we are testing, you should mention your own Eth wallet address
+    const myAddress="0xa8F158Be9e6F780439D135ed0c7B1E21497a9e9C";
+    console.log("horoscopeNFT deployed to:", hscp.address);   
+
+    let txn = await hscp.mintNFT(myAddress, 'virgo');
+    await txn.wait();
+
+ } 
+
+main()
+    .then(() => process.exit(0))  
+    .catch((error) => {    
+    console.error(error);
+    process.exit(1); 
+ });
